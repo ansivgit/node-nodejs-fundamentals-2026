9532eb2eb3ab39d39e03f45efac09eec8ebc65f5
ansivgit <ansiv@yandex.ru>
2026-07-27 16:58:12 +0300

    feat: add snapshot

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,52 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readdir, readFile, stat, writeFile } from 'node:fs/promises';
+
+const ERR_MSG = 'FS operation failed';
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const basePath = __dirname.split(path.sep).splice(0, 3).join(path.sep);
+  // const pathToWorkspace = path.join(__dirname, '..'); //* path to src
+  const pathToWorkspace = path.join(basePath, 'workspace');
+
+  const entries = [];
+
+  try {
+    const entities = await readdir(pathToWorkspace, { recursive: true, withFileTypes: true });
+
+    for (const entity of entities)
+      if (entity.isDirectory()) {
+        entries.push({ path: entity.name, type: 'directory' });
+      } else if (entity.isFile()) {
+        const fileName = entity.name;
+
+        const fullPath = path.join(entity.parentPath, fileName);
+        const relFilePath = path.relative(pathToWorkspace, fullPath);
+
+        const info = await stat(fullPath);
+
+        const buffer = await readFile(fullPath);
+        const base64 = buffer.toString('base64');
+
+        entries.push({ path: relFilePath, type: 'file', size: info.size, content: base64 });
+      }
+
+      const result = {
+        "rootPath": pathToWorkspace,
+        "entries": entries,
+      };
+
+      console.info(result);
+
+      await writeFile(
+        path.join(pathToWorkspace, 'snapshot.json'),
+        JSON.stringify(result));
+  } catch (err) {
+    throw new Error(ERR_MSG, err);
+  }
 };
 
 await snapshot();
