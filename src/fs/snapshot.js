2b258e2b572b200c4d63c6f65ce2829e29940535
ansivgit <ansiv@yandex.ru>
2026-07-28 16:50:37 +0300

    fix: clean code

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,15 +1,11 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { readdir, readFile, stat, writeFile } from 'node:fs/promises';
 
-const ERR_MSG = 'FS operation failed';
+import { getBasePath } from '../utils.js';
+import { ERR_MSG } from '../constants.js';
 
 const snapshot = async () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  const basePath = __dirname.split(path.sep).splice(0, 3).join(path.sep);
-  // const pathToWorkspace = path.join(__dirname, '..'); //* path to src
+  const basePath = getBasePath();
   const pathToWorkspace = path.join(basePath, 'workspace');
 
   const entries = [];
@@ -17,33 +13,42 @@ const snapshot = async () => {
   try {
     const entities = await readdir(pathToWorkspace, { recursive: true, withFileTypes: true });
 
-    for (const entity of entities)
-      if (entity.isDirectory()) {
-        entries.push({ path: entity.name, type: 'directory' });
-      } else if (entity.isFile()) {
-        const fileName = entity.name;
+    console.info(entities);
 
-        const fullPath = path.join(entity.parentPath, fileName);
-        const relFilePath = path.relative(pathToWorkspace, fullPath);
+    for (const entity of entities) {
+      const fileName = entity.name;
+      const fullPath = path.join(entity.parentPath, fileName);
+      const relEntityPath = path.relative(pathToWorkspace, fullPath);
+
+      if (entity.isDirectory()) {
+        entries.push({ path: relEntityPath, type: 'directory' });
+      }
+
+      if (entity.isFile()) {
+        //! Skip .DS_Store and snapshot.json files - remove for cross-check
+        if (fileName === '.DS_Store' || fileName === 'snapshot.json') {
+          continue;
+        }
 
         const info = await stat(fullPath);
 
         const buffer = await readFile(fullPath);
         const base64 = buffer.toString('base64');
 
-        entries.push({ path: relFilePath, type: 'file', size: info.size, content: base64 });
+        entries.push({ path: relEntityPath, type: 'file', size: info.size, content: base64 });
       }
+    }
 
-      const result = {
-        "rootPath": pathToWorkspace,
-        "entries": entries,
-      };
+    const result = {
+      rootPath: pathToWorkspace,
+      entries,
+    };
 
-      console.info(result);
+    console.info(result);
 
-      await writeFile(
-        path.join(pathToWorkspace, 'snapshot.json'),
-        JSON.stringify(result));
+    await writeFile(
+      path.join(pathToWorkspace, 'snapshot.json'),
+      JSON.stringify(result));
   } catch (err) {
     throw new Error(ERR_MSG, err);
   }
