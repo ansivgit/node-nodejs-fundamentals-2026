40220d7a42534b6446bb8544c128574c524340d7
ansivgit <ansiv@yandex.ru>
2026-07-30 20:14:04 +0300

    feat: add cli 1st task

--- src/cli/interactive.js
+++ src/cli/interactive.js
@@ -1,8 +1,42 @@
+import { createInterface } from 'node:readline/promises';
+import { cwd, uptime } from 'node:process';
+
 const interactive = () => {
-  // Write your code here
-  // Use readline module for interactive CLI
-  // Support commands: uptime, cwd, date, exit
-  // Handle Ctrl+C and unknown commands
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('line', (line) => {
+    getHandler(line);
+    rl.prompt();
+  })
+  .on('close', () => {
+    getHandler('exit');
+  });
 };
+
+const getHandler = (line) => {
+  const command = line.trim();
+
+  switch (command) {
+    case 'uptime':
+      console.info(Math.floor(uptime() * 100) / 100);
+      break;
+    case 'cwd':
+      console.info(cwd());
+      break;
+    case 'date':
+      console.info(new Date().toISOString());
+      break;
+    case 'exit':
+      console.info('Goodbye!');
+      process.exit(0);
+      break;
+    default:
+      console.info('Unknown command');
+      break;
+  }
+}
 
 interactive();
