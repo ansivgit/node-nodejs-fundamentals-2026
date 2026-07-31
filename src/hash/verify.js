0fa7af2a20e369d1306101c2efeb574c3fc7d1a7
ansivgit <ansiv@yandex.ru>
2026-07-31 19:12:34 +0300

    feat: add hash task

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,40 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import { pipeline } from 'node:stream/promises';
+import { ERR_MSG } from '../constants.js';
+
+const getFileHash = async (filepath) => {
+  const hash = createHash('sha256');
+  const readableStream = createReadStream(filepath);
+
+  await pipeline(readableStream, hash);
+
+  return hash.digest('hex');
+}
+
 const verify = async () => {
-  // Write your code here
-  // Read checksums.json
-  // Calculate SHA256 hash using Streams API
-  // Print result: filename — OK/FAIL
+  let data;
+
+  try {
+    data = JSON.parse(await readFile('./checksums.json', 'utf8'));
+  } catch {
+    throw new Error(ERR_MSG);
+  }
+
+  for(const [filename, expectedHash] of Object.entries(data)) {
+    try {
+      const actualHash = await getFileHash(`./files/${filename}`);
+
+      if (actualHash === expectedHash) {
+        console.info(`${filename} — OK`);
+      } else {
+        console.info(`${filename} — FAIL`);
+      }
+    } catch (err) {
+      console.info(`${filename} — FAIL`);
+    }
+  }
 };
 
 await verify();
