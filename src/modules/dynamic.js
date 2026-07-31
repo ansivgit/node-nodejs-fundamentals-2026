ccddded4c5578af82ebc99cf482c474a436c0102
ansivgit <ansiv@yandex.ru>
2026-07-31 15:14:40 +0300

    feat: add modules 1st task v1

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,47 @@
+import {readdir} from "node:fs/promises";
+
 const dynamic = async () => {
-  // Write your code here
-  // Accept plugin name as CLI argument
-  // Dynamically import plugin from plugins/ directory
-  // Call run() function and print result
-  // Handle missing plugin case
+  try {
+    const entities = await readdir('src/modules/plugins', { withFileTypes: true });
+    const args = process.argv.slice(2).filter((arg) => !arg.startsWith('-'));
+    const plugins = new Set();
+    let currentPlugin = '';
+
+    if (!args.length) {
+      console.info('Plugin not found');
+      process.exit(1);
+    }
+
+    for (const entity of entities) {
+      const fileName = entity.name.slice(0, -3);
+
+      if (!entity.isFile()) {
+        console.info('Plugin not found');
+        process.exit(1);
+      }
+
+      plugins.add(fileName);
+    }
+
+    plugins.forEach((plugin) => {
+      if (args[0] === plugin || args[0] === `${plugin}.js`) {
+        currentPlugin = plugin;
+      }
+    })
+    // console.log(`Current plugin: ${currentPlugin}`);
+
+    if (!currentPlugin) {
+      console.info('Plugin not found');
+      process.exit(1);
+    }
+
+    const pluginFunction = await import(`./plugins/${currentPlugin}.js`);
+    console.info(pluginFunction.run());
+
+    process.exit(1);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await dynamic();
