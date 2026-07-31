483999a6402b9589b312222393a00a7d1ae5c8d7
ansivgit <ansiv@yandex.ru>
2026-07-31 15:25:46 +0300

    feat: add modules 1st task v2

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,46 +1,22 @@
-import {readdir} from "node:fs/promises";
-
 const dynamic = async () => {
+    let pluginName = process.argv.slice(2).filter((arg) => !arg.startsWith('-'))[0];
+
+    if (!pluginName) {
+      console.info('Plugin not found');
+      process.exit(1);
+    }
+
+    if (!pluginName.endsWith('.js')) {
+      pluginName = `${pluginName}.js`;
+    }
   try {
-    const entities = await readdir('src/modules/plugins', { withFileTypes: true });
-    const args = process.argv.slice(2).filter((arg) => !arg.startsWith('-'));
-    const plugins = new Set();
-    let currentPlugin = '';
-
-    if (!args.length) {
-      console.info('Plugin not found');
-      process.exit(1);
-    }
-
-    for (const entity of entities) {
-      const fileName = entity.name.slice(0, -3);
-
-      if (!entity.isFile()) {
-        console.info('Plugin not found');
-        process.exit(1);
-      }
-
-      plugins.add(fileName);
-    }
-
-    plugins.forEach((plugin) => {
-      if (args[0] === plugin || args[0] === `${plugin}.js`) {
-        currentPlugin = plugin;
-      }
-    })
-    // console.log(`Current plugin: ${currentPlugin}`);
-
-    if (!currentPlugin) {
-      console.info('Plugin not found');
-      process.exit(1);
-    }
-
-    const pluginFunction = await import(`./plugins/${currentPlugin}.js`);
-    console.info(pluginFunction.run());
+    const plugin = await import(`./plugins/${pluginName}`);
+    console.info(plugin.run());
 
     process.exit(1);
   } catch (err) {
-    console.error(err);
+    console.info('Plugin not found');
+    process.exit(1);
   }
 };
 
