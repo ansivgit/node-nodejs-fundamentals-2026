4a578649f8d2534d40c2c17e073bf2af77f4e1a8
ansivgit <ansiv@yandex.ru>
2026-08-05 19:45:37 +0300

    feat: 2nd part of streams task

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,39 @@
+import { Transform } from 'node:stream';
+
 const filter = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Filter lines by --pattern CLI argument
-  // Use Transform Stream
-  // Write to process.stdout
+  let tail = '';
+  const args = process.argv.slice(2);
+
+  if (args.length && args[0] !== '--pattern') {
+    throw new Error('Unknown arguments');
+  }
+
+  const pattern = args.length ? args.slice(1).join(' ') : '';
+
+  const transformStream = new Transform({
+    transform(chunk, _, callback) {
+      tail += chunk.toString();
+      const lines = tail.split(/\\n|\r?\n/);
+      tail = lines.pop() ?? '';
+
+      const out = lines
+        .filter((line) => line.includes(pattern))
+        .map((line) => `${line}\n`)
+        .join('\n');
+
+      callback(null, out);
+    },
+
+    flush(callback) {
+      if (tail.length > 0 && tail.includes(pattern)) {
+        callback(null, `${tail}\n`);
+        return;
+      }
+      callback();
+    },
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout).pipe(process.stderr);
 };
 
 filter();
