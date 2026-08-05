b84dd9217628638805b83ab8e52559bd6a6d2b54
ansivgit <ansiv@yandex.ru>
2026-08-05 18:58:42 +0300

    feat: 1st part of streams task

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,32 @@
+import { Transform } from 'node:stream';
+
+let tail = '';
+let lineNumber = 1;
+
+const transformStream = new Transform({
+  transform(chunk, _, callback) {
+    tail += chunk.toString();
+    const lines = tail.split(/\\n|\r?\n/);
+    tail = lines.pop() ?? '';
+
+    const out = lines
+      .map((line) => `${lineNumber++} | ${line}\n`)
+      .join('');
+
+    callback(null, out);
+  },
+
+  flush(callback) {
+    if (tail.length > 0) {
+      callback(null, `${lineNumber} | ${tail}\n`);
+      return;
+    }
+    callback();
+  },
+});
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 lineNumberer();
